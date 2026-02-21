8b1ca3a5f7a14841235894d9ce47208bcb50ec2c
Chani Avramovich <c050411801@gmail.com>
2026-02-21 21:43:32 +0200

    Opload part 5

--- inquirymanager_system/src/Main.java
+++ inquirymanager_system/src/Main.java
@@ -1,28 +1,35 @@
 import business.InquiryHandling;
+import business.InquiryManager;
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
 
-        InquiryHandling handling1 = new InquiryHandling();
-        Thread handling2 = new InquiryHandling();
-        InquiryHandling handling3 = new InquiryHandling();
-        InquiryHandling handling4 = new InquiryHandling();
-        handling1. createInquiry();
-        ((InquiryHandling) handling2).createInquiry();
-        handling3.createInquiry();
-        handling4.createInquiry();
 
-        handling1.start();
-        handling2.start();
-        handling3.start();
-        handling4.start();
+     //   InquiryHandling handling1 = new InquiryHandling();
+        //Thread handling2 = new InquiryHandling();
+       // InquiryHandling handling3 = new InquiryHandling();
+      //  InquiryHandling handling4 = new InquiryHandling();
+     //   handling1. createInquiry();
+        //((InquiryHandling) handling2).createInquiry();
+      //  handling3.createInquiry();
+       // handling4.createInquiry();
+
+       // handling1.start();
+        //handling2.start();
+      //  handling3.start();
+       // handling4.start();
 
        // handling1.run();
       //  handling2.run();
      //   handling3.run();
         //handling4.run();
+
+        InquiryManager inquiryManager = new InquiryManager();
+        inquiryManager.inquiryCreation();
+        inquiryManager.processInquiryManager();
+
 
 
 
--- inquirymanager_system/src/business/InquiryManager.java
+++ inquirymanager_system/src/business/InquiryManager.java
@@ -1,12 +1,61 @@
 package business;
 
+import Data.Complaint;
+import Data.Inquiry;
+import Data.Question;
+import Data.Request;
+import business.InquiryHandling;
+
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class InquiryManager {
 
-    Queue q=new LinkedList();
+    private Queue<Inquiry> inquiryQueue = new LinkedList<Inquiry>();
+    private Inquiry currentInquiry;
 
 
+    public void inquiryCreation( ) {
+        int x;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Please press the appropriate key.");
+        System.out.println("1-question 2-request 3-complain");
+        x = sc.nextInt();
+        switch (x) {
+            case 1:
+                currentInquiry = new Question();
+                break;
+            case 2:
+                currentInquiry = new Request();
+                break;
+            case 3:
+                currentInquiry = new Complaint();
+                break;
+            default:
+                System.out.println("Inactive key, please press again.");
+                return;
+        }
+        currentInquiry.fillDataByUser();
+        inquiryQueue.add(currentInquiry);
+
+    }
+
+    public void processInquiryManager() {
+
+        while (!inquiryQueue.isEmpty()) {
+
+            Inquiry inquiry = inquiryQueue.poll();
+
+            InquiryHandling inquiryHandling = new InquiryHandling(inquiry);
+
+            inquiryHandling.start();
+
+        }
+    }
 
 }
+
+
+
+
